0dca85b5cb573fb381029343149599f942dce0c0
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-24 22:08:44 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -18,9 +18,9 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 60))
+    asyncio.create_task(delete_after_delay(bot, sent_message, 60))
 
-async def delete_after_delay(message, delay):
+async def delete_after_delay(bot, message, delay):
     text = await bot.send_message(message.chat.id, "[𝐅𝐢𝐥𝐞𝐬 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐃𝐞𝐥𝐞𝐭𝐞𝐝 𝐢𝐧 𝟑𝟎 𝐦𝐢𝐧𝐮𝐭𝐞𝐬](https://t.me/The_Silent_Teams)", disable_web_page_preview=True)
     await asyncio.sleep(delay)
     await message.delete()
