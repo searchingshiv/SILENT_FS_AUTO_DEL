22e2678993ace5c6effbe3fe5d93a856703f07f6
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-24 22:22:33 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -11,17 +11,16 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_ids=file_id)
-
+        text = await bot.send_message(message.chat.id, "[𝐅𝐢𝐥𝐞𝐬 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐃𝐞𝐥𝐞𝐭𝐞𝐝 𝐢𝐧 𝟑𝟎 𝐦𝐢𝐧𝐮𝐭𝐞𝐬](https://t.me/The_Silent_Teams)", disable_web_page_preview=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return await media_forward(bot, user_id, file_id)
 
-async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int,text):
     sent_message = await media_forward(bot, user_id, file_id)
-    asyncio.create_task(delete_after_delay(bot, sent_message, 60))
+    asyncio.create_task(delete_after_delay( sent_message, 60,text))
 
-async def delete_after_delay(bot, message, delay):
-    text = await bot.send_message(message.chat.id, "[𝐅𝐢𝐥𝐞𝐬 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐃𝐞𝐥𝐞𝐭𝐞𝐝 𝐢𝐧 𝟑𝟎 𝐦𝐢𝐧𝐮𝐭𝐞𝐬](https://t.me/The_Silent_Teams)", disable_web_page_preview=True)
+async def delete_after_delay( message, delay,text):
     await asyncio.sleep(delay)
     await message.delete()
     await text.delete()
