fc7bc1f27b615d23c2a4cc86ee861fcdbef53f9a
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-24 12:59:17 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -23,8 +23,9 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 14400))
+    asyncio.create_task(delete_after_delay(sent_message, 1800))
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
+    await message.delete()
     await message.delete()
